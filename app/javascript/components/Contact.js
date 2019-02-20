6393e57d8ab9bc48d77eaffa2bdc5c0aaa5de02a
IsBennani <is.bennani@gmail.com>
2019-02-20 15:05:58 +0000

    add js, yarn, and paths

---
+++ app/javascript/components/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class Contact extends React.Component {
+  render (){
+    return(
+      <div> OK this is the Contact page </div> 
+      )
+  }
+
+}
+export default Contact 
